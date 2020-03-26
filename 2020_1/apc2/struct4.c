edf9dafda1eb9174992e68f2868b2d4fc95a388d
rambim <rambim@gmail.com>
2020-03-25 21:08:38 -0300

    exerciciostruct1

--- 2020_1/apc2/struct4.c
+++ 2020_1/apc2/struct4.c
@@ -7,25 +7,32 @@ typedef struct aluno
     int valor2;
 } aluno;
 
-int dobrar(aluno a);
+void dobrar1(aluno a);
+void dobrar2(aluno *a);
 
 int main()
 {
     aluno a;
-    int x;
+
     a.valor1 = 2;
     a.valor2 = 3;
-    x = dobrar(a);
 
-    printf("\n VAlor de x = %d", x);
-
-    printf("\n Fora da funcao: ");
+    dobrar1(a);
+    printf("\n");
+    printf("\n Fora da funcao: depois de dobrar 1");
     printf("\n Valor 1  = %d valor 2  = %d", a.valor1, a.valor2);
+
+    printf("\n");
+    dobrar2(&a);
+    printf("\n Fora da funcao: depois de dobrar ");
+    printf("\n Valor 1  = %d valor 2  = %d", a.valor1, a.valor2);
+    printf("\n");
+
 
     return 0;
 }
 
-int dobrar(aluno a)
+void dobrar1(aluno a)
 {
     a.valor1 = a.valor1 * 2;
     a.valor2 = a.valor2 * 2;
@@ -33,5 +40,12 @@ int dobrar(aluno a)
     printf("\n Dentro da funcao: ");
     printf("\n Valor 1  = %d valor 2  = %d", a.valor1, a.valor2);
     printf("\n saindo da funcao ");
-    return 33;
+}
+
+void dobrar2(aluno *a)
+{
+    // (*a).valor1 = (*a).valor1 * 2;
+    // (*a).valor2 = (*a).valor2 * 2;
+        a->valor1 = a->valor1 *2;
+        a->valor2 = a->valor2 *2;
 }
