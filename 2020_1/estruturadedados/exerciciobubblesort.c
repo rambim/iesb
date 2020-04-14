3ae5e90bd17930baf7a3ee7f359341a5e600efb0
rambim <rambim@gmail.com>
2020-04-14 19:37:20 -0300

    trabalho prog paralela

---
+++ 2020_1/estruturadedados/exerciciobubblesort.c
@@ -0,0 +1,14 @@
+/*
+
+Mostre o tempo de ordenação de um vetor aleatório com os seguintes tamanhos:
+5 testes pra cada tamanho
+
+10
+100
+1000
+10000
+100000
+1000000 vai demorar muito!
+
+*/
+
---
+++ 2020_1/projeto_integrador/GRUPO R/trabalhop1.c
@@ -0,0 +1,39 @@
+/*
+
+Faça um um programa serial (não paralelo) que receba por parâmetro o nome de um arquivo
+e uma string de tamanho máximo de 100 caracteres.
+
+Seu programa deve procurar a string no arquivo e mostrar
+quantas ocorrências da string há no arquivo.
+
+A avaliação será feita em arquivos de diferentes tamanhos:
+
+10k     
+100k    
+1mb     
+100mb   
+1gb     
+10gb    
+50gb    
+
+Gerar relatório com a média do tempo de 5 execuções para cada arquivo com a solução serial ( não paralela).
+
+Implementar a mesma solução de forma paralela e gerar os seguintes relatórios com
+a média do tempo de 5 execução com 1,2,3,4,5 e 6 threads.
+
+Data da entrega: 19/04/2020 as 18:00 horas;
+
+Execução Serial + paralela arquivo de 10k   (1 ponto)
+Execução Serial + paralela arquivo de 100k  (1 ponto)
+Execução Serial + paralela arquivo de 1mb   (1.5 ponto)
+Execução Serial + paralela arquivo de 100mb (1.5 ponto)
+Execução Serial + paralela arquivo de 1gb   (2 pontos)
+Execução Serial + paralela arquivo de 10gb  (3 pontos)
+
+
+
+
+*/
+
+
+
