80503803c92bfb06f429423f682cbf0d3db2e232
rambim <rambim@gmail.com>
2020-04-24 21:51:19 -0300

    aula 24042020

---
+++ 2020_1/analise_de_algoritmo/exerciciotabeladinamica1.c
@@ -0,0 +1,24 @@
+/*
+TODAS AS SOLUÇÕES DEVEM SER IMPLEMENTADAS EM C 
+DATA DE ENTREGA: 30/04/2020 AS 23:59!!!!
+
+
+a) calcular o tempo médio (5 execuções) de 100.000.000 de inserções e 100.000.000 
+de remoções em um vetor de tamanho fixo de 100.000.000
+
+b) calcular o tempo médio (5 execuções) de 100.000.000 de inserções e 100.000.000 
+de remoções em um vetor dinâmico. Com aumento * 2 e diminuição /2.
+
+c) calcular o tempo médio (5 execuções) de 100.000.000 de inserções e 100.000.000 
+de remoções em um vetor dinâmico. Com aumento * 3 e diminuição /3.
+
+d) Qual a complexidade de tempo das alternativas A,B e C.
+e) Dobrar ao invés de triplicar o tamanho  na tabela dinâmica
+é pior ou melhor ? Em qual sentido? Justifique.
+
+f) faça a dedução matemática da complexidade da utilização da tabela dinâmica.
+
+*/
+
+
+
