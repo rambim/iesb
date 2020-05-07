67fbcb4c1cf7371ef8a34a150b919115240d4ca5
rambim <rambim@gmail.com>
2020-05-06 22:00:31 -0300

    aula06052020

--- 2020_1/apc2/exercicios/exercicioarquivo1.c
+++ 2020_1/apc2/exercicios/exercicioarquivo1.c
@@ -1,14 +1,14 @@
 /*
 
-1)
+
 Faça um programa que receba do usuário um número N inteiro
 positivo e guarde em um arquivo de nome "resposta.txt"
 o dobro de seu valor
 
-2)
-Faça um programa que receba do usuário sua idade e guarde em
-um arquivo de nome "primos.txt" todos números primos entre
-1 e a idade do usuário.
+*/
+
+
+
 
 
 
