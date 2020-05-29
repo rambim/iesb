d3d1fcb8179f933217b4f75893400e1bf11017e0
rambim <rambim@gmail.com>
2020-05-29 18:58:15 -0300

    aula29052020

---
+++ 2020_1/analise_de_algoritmo/exercicios/exerciciotexto.c
@@ -0,0 +1,30 @@
+/*
+Fazer a leitura e interpretação da seguinte parte teórica
+
+Busca de uma palavra em um texto
+    O problema
+    Algoritmo inocente
+    Maior prefixo de P que é sufixo de T
+    Algoritmo do autômato
+    Correção do algoritmo
+    Consumo de tempo
+    Pré-processamento
+    Consumo de tempo total
+
+Resolver os exercícios:
+    Exercício 1.1
+    Exercício 1.2
+    Exercício 1.3
+    Exercício 2.1
+    Exercício 2.2
+    Exercício 3.1
+
+Implemente a Solução Inocente a do Autômato.
+
+Link https://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/string-match.html
+
+*/
+
+
+
+
