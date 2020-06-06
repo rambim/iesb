3f6ad981bf60a974ea100a2cf64165a793ad8cea
rambim <rambim@gmail.com>
2020-06-05 21:32:09 -0300

    05062020

--- 2020_1/analise_de_algoritmo/exercicios/exerciciotexto.c
+++ 2020_1/analise_de_algoritmo/exercicios/exerciciotexto.c
@@ -1,5 +1,6 @@
 /*
 Fazer a leitura e interpretação da seguinte parte teórica
+Link https://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/string-match.html
 
 Busca de uma palavra em um texto
     O problema
@@ -11,17 +12,42 @@ Busca de uma palavra em um texto
     Pré-processamento
     Consumo de tempo total
 
-Resolver os exercícios:
+
+1)  Implemente a Solução Inocente
+
+2)  Resolver os exercícios:
     Exercício 1.1
     Exercício 1.2
     Exercício 1.3
+
+3)  Implemente a Solução de BoyerMore (Hospool)
+
+    Cenas dos próximos capitulos ( Sunday)
+    
+    
+
+4)  Resolver os exercícios:
+    a)...
+    b)...
+    c)...
+
+5)  Implemente a solução KMP
+
+6)  Resolver os exercícios:
+    a)...
+    b)...
+    c)...
+
+Questões BONUS
+
+7)  Implemente a busca de string com automatos inclusive seu pré processamento.
+
+8)  Resolver os exercícios:
     Exercício 2.1
     Exercício 2.2
     Exercício 3.1
 
-Implemente a Solução Inocente a do Autômato.
 
-Link https://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/string-match.html
 
 */
 
