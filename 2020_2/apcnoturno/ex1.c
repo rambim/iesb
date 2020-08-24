2cb7b59bc8283c4b1b1141515418a2d62676f286
rambim <rambim@gmail.com>
2020-08-24 19:41:56 -0300

    24042020

---
+++ 2020_2/apcnoturno/ex1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void funcao1(int x);
+int main()
+{
+    int x=2;
+    printf("\n%d",x);
+    funcao1(x);
+    printf("\n%d",x);
+    printf("\n");
+    return 0;
+}
+
+void funcao1(int x)
+{
+    x = 10;
+    printf("\n%d",x);
+}
+
+
+ALEXANDRE CUNHA
+CLEDINILTON OLIVEIRA
+HENRIQUE SILVA
+ISRAEL MORAES
+JOSÉ GIANNELLI
+RAFAEL PINHEIRO
