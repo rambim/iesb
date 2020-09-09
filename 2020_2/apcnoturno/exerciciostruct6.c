c4c297a9ea058d3ee2f38db3222b1510978825e2
Felippe <felippe.giuliani@automatizai.com.br>
2020-09-09 20:52:36 -0300

    aula 09092020 noturno

---
+++ 2020_2/apcnoturno/exerciciostruct6.c
@@ -0,0 +1,14 @@
+/*
+
+Faça um programa que gerencie o estoque de um mercado e:
+•Crie e leia um vetor de 5 produtos, com os dados:  código (inteiro), preço e quantidade.
+•Leia  um  pedido,  composto  por  um  código  de  produto  e  a  quantidade.   
+Localizeeste  código  no  vetor  e,  se  houver  quantidade  suficiente  para  atender  ao  pedido
+integralmente, atualize o estoque e informe o usuário. 
+Repita este processo at ́e ler um código igual a zero.
+Se por algum motivo nãoo for poss ́ıvel atender ao pedido, mostre uma mensagem informando 
+qual erro ocorreu.
+
+Os produtos devem ser amarmazenados em um vetor de structs
+*/
+
