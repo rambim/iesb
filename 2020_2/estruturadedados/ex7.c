e6d51c7d4dc604d0b38b333c8e03ec00070cdc22
Felippe <felippe.giuliani@automatizai.com.br>
2020-09-14 21:33:10 -0300

    aula 14092020

--- 2020_2/estruturadedados/ex7.c
+++ 2020_2/estruturadedados/ex7.c
@@ -16,27 +16,57 @@ typedef struct registro
 registro *aloca_registro();
 lista *aloca_lista();
 void incluir_na_lista(lista *l, int x);
-void mostrar(lista * l);
+void mostrar(lista *l);
+void remove_na_lista(lista *l, int x);
+void remove_todos_na_lista(lista *l, int x);
 
 int main()
 {
-    lista * lista_do_felippe;
-    int numero=0;
+    lista *lista_do_felippe;
+    int numero = 0;
 
     lista_do_felippe = aloca_lista();
 
-    while(numero!=-1)
+    int opcao;
+
+    do
     {
-        printf("\n Digite um numero para ser includo na lista: ");
-        scanf("%d",&numero);
-        incluir_na_lista(lista_do_felippe,numero);
-    }
+        printf("\n 1 - incluir:");
+        printf("\n 2 - Remover ");
+        printf("\n 3 - Mostrar lista");
+        printf("\n 4 - remove repetidos");
+        printf("\n 5 - Sair");
+        scanf("%d", &opcao);
 
-    mostrar(lista_do_felippe);   
+        switch (opcao)
+        {
+        case 1:
+            printf("\n Digite um numero para ser includo na lista: ");
+            scanf("%d", &numero);
+            incluir_na_lista(lista_do_felippe, numero);
+            break;
+        case 2:
+            printf("\n Digite um numero para ser removido na lista: ");
+            scanf("%d", &numero);
+            remove_na_lista(lista_do_felippe, numero);
+            break;
+        case 3:
+            mostrar(lista_do_felippe);
+            break;
+        case 4:
+            printf("\n Digite um numero para ser removido na lista: ");
+            scanf("%d", &numero);
+            remove_todos_na_lista(lista_do_felippe, numero);
+            break;
 
-
-
-    return 0;
+        case 5:
+            printf("\n Saindo do programa");
+            break;
+        default:
+            printf("\n Opcao invalida");
+            break;
+        }
+    } while (opcao != 5);
 }
 
 lista *aloca_lista()
@@ -83,23 +113,108 @@ void incluir_na_lista(lista *l, int x)
     l->qtd++;
 }
 
-void mostrar(lista * l)
+void mostrar(lista *l)
 {
-    registro * aux;
+    registro *aux;
 
-    if ( l->qtd==0)
+    if (l->qtd == 0)
     {
         printf("\n Lista vazia");
     }
     else
     {
-        aux= l->inicio;
-        while(aux!=NULL)
+        aux = l->inicio;
+        while (aux != NULL)
         {
-            printf("\n%d",aux->valor);
+            printf("\n%d", aux->valor);
             aux = aux->prox;
         }
-
     }
-    
+}
+
+void remove_na_lista(lista *l, int x)
+{
+
+    registro *welisson, *morcego;
+
+    if (l->inicio == NULL)
+    {
+        printf("\n lista vazia");
+        return;
+    }
+    welisson = l->inicio;
+
+    while (welisson != NULL)
+    {
+        if (welisson->valor == x)
+        {
+            if (morcego == NULL)
+            {
+                l->inicio = welisson->prox;
+            }
+            else
+            {
+                morcego->prox = welisson->prox;
+            }
+
+            free(welisson);
+            l->qtd--;
+            printf("\n Removido com sucesso");
+            return;
+        }
+        else
+        {
+            morcego = welisson;
+            welisson = welisson->prox;
+        }
+    }
+    printf("\n Elemento nao foi encontrado na lista");
+
+    return;
+}
+
+void remove_todos_na_lista(lista *l, int x)
+{
+
+    registro *welisson, *morcego;
+    int apagou = 0;
+
+    if (l->inicio == NULL)
+    {
+        printf("\n lista vazia");
+        return;
+    }
+    welisson = l->inicio;
+
+    while (welisson != NULL)
+    {
+        if (welisson->valor == x)
+        {
+            if (morcego == NULL)
+            {
+                l->inicio = welisson->prox;
+            }
+            else
+            {
+                morcego->prox = welisson->prox;
+            }
+
+            free(welisson);
+            l->qtd--;
+            printf("\n Removido com sucesso");
+            apagou = 1;
+            welisson = morcego->prox;
+        }
+        else
+        {
+            morcego = welisson;
+            welisson = welisson->prox;
+        }
+    }
+    if (!apagou)
+    {
+        printf("\n Elemento nao foi encontrado na lista");
+    }
+
+    return;
 }
---
+++ 2020_2/estruturadedados/exerciciolistaligada1.c
@@ -0,0 +1,7 @@
+/* Construa um programa que leia do usuário 10 valores inteiros;
+armazene os valores pares em uma lista ligada de nome lista_de_pares e os números ímpares em uma lista
+de nome lista_de_ímpares;
+
+Depois de preenchida as 2 listas, diga qual das 2 listas é a maior e mostre seus valores;
+
+*/
