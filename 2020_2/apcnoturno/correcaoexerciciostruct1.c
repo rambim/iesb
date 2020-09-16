d730cf522b80c3f1add2f43a0b0f2e78f5a7680c
Felippe <felippe.giuliani@automatizai.com.br>
2020-09-16 19:53:51 -0300

    teste

--- 2020_2/apcnoturno/correcaoexerciciostruct1.c
+++ 2020_2/apcnoturno/correcaoexerciciostruct1.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 // complete as funções restantes!
 
 typedef struct aluno
 {
-   int matricula;
-   float notas[3];
+    int matricula;
+    float notas[3];
 
-}aluno;
-void LeAluno(aluno * dados_de_um_aluno);
+} aluno;
+void LeAluno(aluno *dados_de_um_aluno);
 
 int main()
 {
     int i;
     aluno Turma[5];
 
-    for(i=0; i< 5; i++)
-    {
-        LeAluno(&Turma[i]);  // função que lê de teclado
-    }
+    // for(i=0; i< 5; i++)
+    // {
+    LeAluno(Turma); // função que lê de teclado
+    // }
 
-    
     // ImprimeTurma(&Turma[0]); Função que imprime os dados de todos os alunos
 
-
-    for(i=0; i< 5; i++)
+    for (i = 0; i < 5; i++)
     {
         // media = CalculaMediaAluno(.......);// função que calcula a média das notas de um aluno
-                                             
-        printf("Aluno %d  - Media = %f\n"); // imprime os dados do aluno
 
+        printf("Aluno %d  - Media = %f\n"); // imprime os dados do aluno
     }
     return 0;
 }
 
-
-void LeAluno(aluno * dados_de_um_aluno)
+void LeAluno(aluno *dados_de_um_aluno)
 {
-    printf("\n Digite a matricula do aluno: ");
-    scanf("%d",&dados_de_um_aluno->matricula);
-    printf("\n Digite a n1: ");
-    scanf("%f",&dados_de_um_aluno->notas[0]);
-    printf("\n Digite a n2: ");
-    scanf("%f",&dados_de_um_aluno->notas[1]);
+    int i;
 
+    for (i = 0; i < 5; i++)
+    {
+
+        printf("\n Digite a matricula do aluno: ");
+        scanf("%d", &dados_de_um_aluno[i].matricula);
+        printf("\n Digite a n1: ");
+        scanf("%f", &dados_de_um_aluno[i].notas[0]);
+        printf("\n Digite a n2: ");
+        scanf("%f", &dados_de_um_aluno[i].notas[1]);
+    }
 }
 
 // ImprimeTurma(Turma); Função que imprime os dados de todos os alunos
