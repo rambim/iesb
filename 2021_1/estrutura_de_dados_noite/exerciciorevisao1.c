7103f11f033f548518ce1e77adcac521b7059930
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-03-01 21:43:18 -0300

    aula continuacao 01032021 noturna

--- 2021_1/estrutura_de_dados_noite/exerciciorevisao1.c
+++ 2021_1/estrutura_de_dados_noite/exerciciorevisao1.c
@@ -1,6 +1,6 @@
 /*
 
-
+1)
 Faça um programa que receba do usuário um número N inteiro positivo e aloque um vetor de cachorros.
 
 Cada cachorro posssui: Nome, Idade, raça, cor, peso e altura.
@@ -12,5 +12,19 @@ B) Qual o peso médio dos cachorros
 C) Qual é o cachorro mais alto
 D) Qual o cachorro mais velho.
 E) Quantos cachorros são maiores do que 30 cm.
+
+
+2) Faça um programa que receba do usuário dois números N e M positivos inteiros do usuário. Aloque um vetor de N números inteiros, 
+preencha os N números. Para cada um dos N números mostre a diferença em relação a M:
+
+Ex:
+    N = 5
+    m = 5
+
+    Vetor de N números:
+    1, 2, 3,4,5
+
+    Resultado:
+    4, 3, 2, 1, 0;
 
 */
