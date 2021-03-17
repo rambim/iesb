f7105c57cd11c24def5a281d76139a300970c21b
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-03-16 22:04:11 -0300

    aula16032021

--- 2021_1/programacao_paralela/exercicio2.c
+++ 2021_1/programacao_paralela/exercicio2.c
@@ -7,3 +7,10 @@ do programa que calcula a integral que resulta em PI sem utilizar um vetor de so
 
 
 
+// 	solução 1	solução 2
+// 1	0,85	0,83
+// 2	1,21	0,44
+// 3	1,47	0,34
+// 4	1,66	0,29
+// 5	1,82	0,25
+// 6	1,9	0,25
