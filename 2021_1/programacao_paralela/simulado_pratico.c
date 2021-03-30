e210ff4b8f5b6d30041c2c3fa52915e98baeadcb
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-03-30 19:36:46 -0300

    aula30032021

---
+++ 2021_1/programacao_paralela/simulado_pratico.c
@@ -0,0 +1,60 @@
+/*
+
+1 - Faça um programa que receba do usuário um número N inteiro positivo e mostre o somatório dos N primeiros números primos maiores 
+que 10000 de forma sequencial e paralela. Evidêncie a melhora de performance na utilização da solução paralela.
+
+
+2 - Faça um programa que receba do usuário um número N  par positivo, aloque um vetor dinâmicamente de tamanho N e preencha as 
+N posições com números aleatórios.
+
+Calcule o somatório dos produtos de cada par de elementos desse vetor de forma sequencial e paralela.
+Evidêncie a melhora de performance na utilização da solução paralela.
+
+Ex:
+
+Vetor: 1,2,3,4,5,6,7,8,9,10
+(1*2) + (3*4) + (5*6) + (7*8) + (9*10)
+
+
+3 - Faça um programa que receba do usuário um valor N e aloque uma matriz NxN.
+Os valores da primeira linha e da primeira coluna devem ser preenchido com valores aleatórios e as demais
+posições da matriz devem ser preenchidas pelo resultado da soma das celulás imediatamente acima e a esquerda da célula que está
+sendo calculada.
+
+Ex:
+01 02 03
+04
+07
+
+02+04 = 06
+
+01 02 03
+04 06
+07
+
+06+03 = 09
+
+01 02 03
+04 06 09
+07
+
+06+07 = 13
+
+01 02 03
+04 06 09
+07 13 
+
+13 + 9 = 21
+
+01 02 03
+04 06 09
+07 13 21
+
+Calcule o somatório da matriz resultante de forma sequencial e paralela.
+Evidêncie a melhora de performance na utilização da solução paralela.
+
+*/
+
+
+
+
