22c12d81de004ffef99c694e2f54feaffa457944
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-04-14 11:03:08 -0300

    aula14042021

--- 2021_1/apc2_manha/exercicio4.c
+++ 2021_1/apc2_manha/exercicio4.c
@@ -5,3 +5,45 @@ Na sua função principal, leia o nome do usuário com até 50 caracteres e most
 quantas e quais vogais há nesse nome:
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio_ext.h>
+
+int vogal(char x);
+int main()
+{
+    char nome[50];
+    int qtd = 0, i;
+
+    printf("\n Digite seu nome: ");
+    __fpurge(stdin);
+    fgets(nome,50,stdin);
+    nome[strlen(nome) - 1] = '\0';
+
+    for (i = 0; i < strlen(nome); i++)
+    {
+        if (vogal(nome[i]))
+        {
+            qtd++;
+            printf("\n vogal: %c", nome[i]);
+        }
+    }
+
+    printf("\n Quantidade de vogais: %d",qtd);
+    printf("\n");
+    return 0;
+}
+
+int vogal(char x)
+{
+    if ((x == 'A') || (x == 'a') ||
+        (x == 'E') || (x == 'e') ||
+        (x == 'I') || (x == 'i') ||
+        (x == 'O') || (x == 'o') ||
+        (x == 'U') || (x == 'u'))
+        return 1;
+    else
+        return 0;
+}
