826edce7c29c5efcce9a1e8c39c9ae73a5bcc6a2
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-05-04 19:35:23 -0300

    aula04052021

---
+++ 2021_1/programacao_paralela/labirinto.c
@@ -0,0 +1,35 @@
+/*
+Valor 1,0 Ponto.
+
+Faça um programa que leia do usuário um arquivo do usuário que irá conter as informações de um labirinto.
+As informações desse arquivo serão dadas da seguinte forma:
+1 = Parede
+2 = caminho livre
+3 = caminho livre com recompensa.
+
+Exemplo de labirinto.
+
+1   1   1   1   1   1   
+1   2   2   2   2   1   
+1   1   1   1   2   1
+1   2   2   2   2   1   
+1   2   1   1   1   1   
+1   2   2   2   3   1   
+1   1   1   1   1   1
+
+Seu programa deverá receber um número N que corresponde a quantidade de threads, N pontos inicias(X,Y) que correspondem 
+as posições iniciais de cada thread e deverá gravar em um arquivo quantos e em quais direções cada thread se movimentou para 
+tentar encontrar a posição da recompensa. Cada thread deverá executar no máximo 50 movimentações. 
+
+Exemplo de arquivo de saida:
+THREAD1;E,D,D,E,C,B;ENCONTROU
+THREAD2;E,D,B,C,C,E;NÃO ENCONTROU
+
+
+*/
+
+
+
+
+
+
