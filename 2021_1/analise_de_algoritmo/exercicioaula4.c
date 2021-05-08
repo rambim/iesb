722dffb9c3e30339b4f764b61f8e4a5af20a0301
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-05-07 21:08:06 -0300

    auala07052021

---
+++ 2021_1/analise_de_algoritmo/exercicioaula4.c
@@ -0,0 +1,18 @@
+/*
+
+1 - Implemente um programa que receba um valor N do usuário e aloque uma string de tamanho N com caracteres aleatórios.
+Além disso, receba do usuário uma  string que corresponde ao padrão que será procurado dentro da string gerada aleatóriamente
+Utilize a solução NAIVE, força bruta.
+
+2 - Prove que a complexidade da implementação NAIVE de busca de string em texto é N² a partir da contagem de instruções
+
+3 - Monte um gráfico que demonstre a curva exponencial para diversos valaores de N e valores de string de busca.
+
+
+4 - Implemente a mesma lógica citada no exercício 1, utilizando o algoritmo KMP.
+
+5 - Prove que a complexidade da implementação KMKP é N+M a partir da contagem de instruções
+
+6 - Monte um gráfico que demonstre a curva lienar para diversos valores de N e vaalores de string de busca.
+
+*/
