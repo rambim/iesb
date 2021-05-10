94b6dce0037321a1366d871e53255ffce0f98779
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-05-10 20:54:02 -0300

    aula noite 10052021

---
+++ 2021_1/estrutura_de_dados_noite/exercicios/lista_exercicio_8.c
@@ -0,0 +1,9 @@
+/*
+
+
+1 - Implemente o algoritmo Quicksort e calcule seu tempo de execução para diversos tamanhos de vetores.
+Monte um gráfico demonstrando a curva de aumento de tempo em relação ao aumento de N ( tamanho do vetor.)
+
+
+*/
+
---
+++ 2021_1/estrutura_de_dados_noite/exercicios/lista_exercicio_9.c
@@ -0,0 +1,10 @@
+
+/*
+1 - Implemente o algoritmo InsertionSort e calcule seu tempo de execução para diversos tamanhos de vetores.
+Monte um gráfico demonstrando a curva de aumento de tempo em relação ao aumento de N ( tamanho do vetor.)
+
+2 - Implemente o algoritmo SelectionSort e calcule seu tempo de execução para diversos tamanhos de vetores.
+Monte um gráfico demonstrando a curva de aumento de tempo em relação ao aumento de N ( tamanho do vetor.)
+
+*/
+
