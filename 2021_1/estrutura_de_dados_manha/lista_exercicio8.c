519d6f9b3ad6cafebbb1f3ee9f7fcb3cf47da6fc
felippe.giuliani <felippe.giuliani@automatizai.com.br>
2021-05-31 21:42:54 -0300

    aula31052021

---
+++ 2021_1/estrutura_de_dados_manha/lista_exercicio8.c
@@ -0,0 +1,7 @@
+/*
+
+Fale qual é o resultado de uma árvore AVL em pré-ordem, em-ordem e pós-ordem a partir da inserção dos seguintes valores:
+
+50, 100, 75, 30 , 10, 5 , 25, 23, 29, 27, 28, 80, 70, 60.
+
+*/
--- 2021_1/projeto_integrador/teste.c
+++ 2021_1/projeto_integrador/teste.c
@@ -2,27 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-void trata_linha ( char * linha, int tam);
-
-int main()
+typedef struct professor
 {
-    char texto[10] = "áa";
+    char nome[30];
+    int matricula;
+    float valor;
+} professor;
+
+int main(void)
+{
+
+    professor *professores, *leitura;
     int i;
 
-    printf("1 - %d 2 - %d",texto[0],texto[1]);
+    professores = (professor *)calloc(10, sizeof(professor));
+    leitura = (professor *)calloc(10, sizeof(professor));
 
-    if (texto[0]==-61)
+    for (i = 0; i < 10; i++)
     {
-        texto[0] = 'a';
-        texto[1] = texto[2];
-        texto[2] = texto[3];
+        strcpy(professores[i].nome, "felippe");
+        professores[i].matricula = 10 * i + rand() % 10;
+        professores[i].valor = rand() % 100;
     }
 
-    // for(i=0;i<strlen(texto);i++)
-    // {
-    //     printf("\nC: %c",texto[i]);
-    // }
-    
-    printf("%s",texto);
+    for (i = 0; i < 10; i++)
+    {
+        printf("\n %d : Nome: %s Matricula: %d Valor : %f", i + 1, professores[i].nome, professores[i].matricula, professores[i].valor);
+    }
+
+    FILE *arq, *arq2;
+
+    arq = fopen("professores.txt", "w");
+
+    if (arq == NULL)
+        printf("\ntreta");
+    else
+        fwrite(professores, 10, sizeof(professor), arq);
+    fclose(arq);
+
+    arq2 = fopen("professores.txt", "r");
+
+    if (arq2 == NULL)
+        printf("\n Problemas para ler aquivo");
+    else
+        fread(leitura, 10, sizeof(professor), arq2);
+    fclose;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("\n %d : Nome: %s Matricula: %d Valor : %f", i + 1, leitura[i].nome, leitura[i].matricula, leitura[i].valor);
+    }
+
     return 0;
 }
