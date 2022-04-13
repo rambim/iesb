c020b29b4d10e164ab0072ba6d61f2b56d76236e
Jão <84878659+O-Recruta@users.noreply.github.com>
2022-04-13 20:24:49 -0300

    Solução
    Possível encurtar, mas acabaria com a legibilidade

--- 2022_1/apc2/revisao_prova/revisao2.c
+++ 2022_1/apc2/revisao_prova/revisao2.c
@@ -1,13 +1,25 @@
 /*
-
-
 Faça um programa que receba 2 notas do usuario, calcule a média aritmética dessas notas
 e mostre na tela se o usuário está aprovado ou reprovado.
-
 Reprovado : entre 0 - 5
 Aprovado  : maior que 5 e menor igual a 10.
-
-
-
-
 */
+
+#include<stdio.h>
+
+int main(){
+    float nota1, nota2, notaFinal;
+
+    printf("Insira sua primeira e segunda notas separadas por espaço\n");
+    scanf("%f %f", &nota1, &nota2);
+
+    notaFinal = (nota1+nota2)/2;
+
+    if (notaFinal > 5 && notaFinal <= 10){
+        printf("Você passou!!\n");
+    }else{
+        printf("Reprovado, faltou tompêrro\n");
+    }
+
+    return 0;
+}
