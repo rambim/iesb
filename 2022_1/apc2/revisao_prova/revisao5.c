ae614e80b6e4b3796de2a26e4548ebf67f9d28ac
Jão <84878659+O-Recruta@users.noreply.github.com>
2022-04-13 21:45:41 -0300

    Update revisao5.c

--- 2022_1/apc2/revisao_prova/revisao5.c
+++ 2022_1/apc2/revisao_prova/revisao5.c
@@ -1,8 +1,22 @@
 /*
-
 Faça um programa que receba do usuário 10 números e mostre na tela o somatório dos números 
 pares
-
-
 */
 
+#include<stdio.h>
+
+int main(){
+    int numeros[10], sum = 0, i;
+    
+    for(i = 0; i < 10; i++){
+        scanf("%d", &numeros[i]);
+    }
+    for(i = 0; i < 10; i++){
+        if(!(numeros[i]%2)){
+            sum+= numeros[i];
+        }
+    }
+    
+    printf("A somatoria eh %d\n", sum);
+    return 0;
+}
