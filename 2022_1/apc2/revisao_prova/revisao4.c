8939dd07c886378e593861ffb2da97ff528226a5
Jão <84878659+O-Recruta@users.noreply.github.com>
2022-04-13 21:41:05 -0300

    Update revisao4.c

--- 2022_1/apc2/revisao_prova/revisao4.c
+++ 2022_1/apc2/revisao_prova/revisao4.c
@@ -1,6 +1,34 @@
 /*
-
 Faça um programa que receba 10 números do usuário e mostre na tela quantos e quais são primos
-
 */
+#include<stdio.h>
 
+int main(){
+    int numeros[10]; //valores
+    int quantosPrimo, i, j, divs;
+    
+    quantosPrimo = 0;
+  
+    for(i = 0; i < 10; i++){
+        scanf("%d", &numeros[i]);
+    }
+    for(i = 0; i < 10; i++){
+        divs = 0;
+        for(j = 2; j <= numeros[i]; j++){
+            if(numeros[i] == 1){
+                quantosPrimo++;
+                printf("1 eh primo\n");
+                break;
+            }
+            if(numeros[i]%j == 0){
+                divs++;
+            }
+        }
+        if (divs <= 1){
+            quantosPrimo++;
+            printf("%d eh primo\n", numeros[i]);
+        }
+    }
+    printf("Ao todo, %d numeros sao primos\n", quantosPrimo);
+    return 0;
+}
