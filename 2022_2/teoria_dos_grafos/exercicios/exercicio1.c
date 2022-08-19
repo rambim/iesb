4f217c041b1eb87b9f18d7f9478006c56247df57
rambim <rambim@gmail.com>
2022-08-19 20:30:49 -0300

    19082022

---
+++ 2022_2/teoria_dos_grafos/exercicios/exercicio1.c
@@ -0,0 +1,13 @@
+/*
+Faça um programa que receba do usuário N pares de valores, onde cada PAR representa uma aresta
+bidirecional entre cada valor do par.
+
+Mostre uma matriz de adjacência que represente esse grafo
+Mostre uma lista de adjacência que represente esse grafo.
+
+1 2
+1 4
+2 5
+3 6
+
+*/
