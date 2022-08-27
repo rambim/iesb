03405f48dce0d79e67a5e477b780b8a4675a4544
rambim <rambim@gmail.com>
2022-08-26 21:09:24 -0300

    26082022

--- 2022_2/teoria_dos_grafos/exercicios/exercicio1.c
+++ 2022_2/teoria_dos_grafos/exercicios/exercicio1.c
@@ -11,3 +11,42 @@ Mostre uma lista de adjacência que represente esse grafo.
 3 6
 
 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrar_matriz(int ** mat, int l , int c);
+
+int main()
+{
+
+    int **mat;
+    int i;
+
+    mat = (int**)malloc(sizeof(int*)*50);
+    for(i=0;i<50;i++)
+    {
+        mat[i] = (int*)malloc(sizeof(int)*50);
+    }
+
+
+    mostrar_matriz(mat,3,3);
+    return 0;
+}
+
+
+void mostrar_matriz(int **mat, int l , int c)
+{
+    int i,j;
+
+    for(i=0;i<l;i++)
+    {
+        
+        for(j=0;j<c;j++)
+        {
+            printf("\t%d",mat[i][j]);
+        }
+        printf("\n");
+    }
+}
