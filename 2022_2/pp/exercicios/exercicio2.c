4fc5fb6b3723224ad1e0d6cb27d44cda8ecc2bd1
rambim <rambim@gmail.com>
2022-09-08 20:56:51 -0300

    08092022

---
+++ 2022_2/pp/exercicios/exercicio2.c
@@ -0,0 +1,5 @@
+// Gere um relatório de performance de uma solução sequencial
+// e paralela para o seguinte problema:
+
+// Mostre quais, quantos e o somatório de todos números primos entre 
+// 100 e 1000000000
