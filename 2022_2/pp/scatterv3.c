21bce2c7088294657722727173230b96131c6c5f
rambim <rambim@gmail.com>
2022-11-10 21:52:54 -0300

    01112022

--- 2022_2/pp/scatterv3.c
+++ 2022_2/pp/scatterv3.c
@@ -3,88 +3,68 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define SIZE 7
+#define TAM 7
 
 int main(int argc, char *argv[])
 {
-    int rank, size;
-    int rec_buf[SIZE];
+    int rank, qtd_processos;
+    int rec_buf[TAM];
     int n, i;
-    int *data;
+    int data[TAM] = {1, 2, 3, 4, 5, 6, 7};
+    int tamanho;
+    int *vet;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &qtd_processos);
 
-    // if (rank == 0)
-    // {
-    //     scanf("%d", &n);
-    //     data = (int *)malloc(sizeof(int) * n);
-    //     for (i = 0; i < n; i++)
-    //     {
-    //         data[i] = i + 1;
-    //     }
-    //     for (i = 0; i < n; i++)
-    //     {
-    //         printf("I = %d : %d\n", i, data[i]);
-    //     }
-    //     printf("Quantidade de threads: %d\n", size);
-    // }
+    int *sendcounts = malloc(sizeof(int) * qtd_processos);
+    int *displs = malloc(sizeof(int) * qtd_processos);
 
-    // MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    // printf("Rank: %d Valor de N: %d\n", rank, n);
-    // printf("Rank: %d Quantiadde de threads: %d\n", rank, size);
-
-    int tam_por_thread = n / size;
-
-    // printf("Rank: %d tamanho por thread %d\n", rank, tam_por_thread);
-
-    int *sendcounts = malloc(sizeof(int) * size);
-    int *displs = malloc(sizeof(int) * size);
-
-    int aux;
     if (rank == 0)
     {
-        aux = n;
-        for (i = size - 1; i>0; i--)
-        {
-            sendcounts[i] = tam_por_thread;
-            aux = aux - sendcounts[i];
-            displs[i] = aux;
-        }
-        sendcounts[i] = n - (tam_por_thread * (size - 1));
-        aux = aux - sendcounts[i];
-        displs[i] = aux;
+        scanf("%d", &tamanho);
+    }
+    MPI_Bcast(&tamanho, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        for (i = 0; i < size; i++)
+    printf("Rank %d = %d\n", rank, tamanho);
+    vet = (int *)malloc(sizeof(int) * tamanho);
+
+    if (rank == 0)
+    {
+        for (i = 0; i < tamanho; i++)
         {
-            printf("Sendount indice %d = %d\n", i, sendcounts[i]);
-            printf("displ indice %d = %d\n", i, displs[i]);
+            vet[i] = i + 1;
         }
     }
 
-    // sendcounts[0] = 2;
-    // sendcounts[1] = 1;
-    // sendcounts[2] = 3;
-    // sendcounts[3] = 1;
+    MPI_Barrier(MPI_COMM_WORLD);
+    int tam_por_thread = TAM / qtd_processos;
+    int tam_ult_thread = TAM - (tam_por_thread * (qtd_processos - 1));
+    for (i = 0; i < qtd_processos - 1; i++)
+    {
+        // printf("\n Thread %d = Tam = %d", i, tam_por_thread);
+        displs[i] = TAM - (i + 1) * tam_por_thread;
+        sendcounts[i] = tam_por_thread;
+    }
 
-    // displs[0] = 5;
-    // displs[1] = 4;
-    // displs[2] = 1;
-    // displs[3] = 0;
+    // printf("\n Thread %d = Tam = %d", i, tam_ult_thread);
+    displs[i] = 0;
+    sendcounts[i] = tam_ult_thread;
 
-    // MPI_Scatterv(&data, sendcounts, displs, MPI_INT, &rec_buf, 7, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(vet, sendcounts, displs, MPI_INT, &rec_buf, tamanho, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // printf("%d: ", rank);
-    // for (int i = 0; i < sendcounts[rank]; i++) {
-    //     printf("%d\t", rec_buf[i]);
-    // }
-    // printf("\n");
+    printf("\n%d: ", rank);
+    for (int i = 0; i < sendcounts[rank]; i++)
+    {
+        printf("%d\t", rec_buf[i]);
+    }
+    printf("\n");
 
     MPI_Finalize();
 
-    // free(sendcounts);
-    // free(displs);
+    free(sendcounts);
+    free(displs);
 
     return 0;
 }
