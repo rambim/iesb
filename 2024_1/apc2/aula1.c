ce533ca8eefe7a731edb569cda73cbb629412660
IESB <rambim@gmail.com>
2024-03-04 21:28:20 -0300

    lista inserir e mostrar

---
+++ 2024_1/apc2/aula1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrar_hello();
+
+
+int main()
+{
+    mostrar_hello();
+    
+    return 0;
+}
+
+void mostrar_hello()
+{
+    printf("Hello world!\n");
+}
